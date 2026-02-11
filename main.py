06ac9ecb72d2c1e37f205707435c701d81212913
RyotaKoba <koba@mprg.cs.chubu.ac.jp>
2026-02-11 17:45:10 +0900

    ch

--- main.py
+++ main.py
@@ -113,6 +113,8 @@ def main():
         init_data = model.state_dict()
         pruner.SCORE = snip(args, model, tokenizer, device)
         # del model
+        model = model.half()
+        pruner.SCORE = pruner.SCORE.half()
         torch.cuda.empty_cache()
         # model = AutoModelForCausalLM.from_pretrained(args.model,torch_dtype=torch.float16,cache_dir=args.cache_dir,device_map="auto")
         # pruner.SCORE = pruner.SCORE.half()
