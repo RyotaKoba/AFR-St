4bc525cb1e2d986d1c68e994aa54a84782d296cc
RyotaKoba <koba@mprg.cs.chubu.ac.jp>
2026-02-11 17:40:24 +0900

    ch: nsample

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ CUDA_LAUNCH_BLOCKING=1 python3 main.py \
 --model $model \
 --prune_method "snip" \
 --pruning_ratio 0.2 \
---nsamples 12 \
+--nsamples 2 \
 --cuda \
 --save_model "./pruned_model/trash" \
 # --global_pruning
