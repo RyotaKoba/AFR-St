6c20a66b086eb51cfff500df2c690826365212ed
RyotaKoba <koba@mprg.cs.chubu.ac.jp>
2026-02-11 15:39:34 +0900

    ch

--- start.sh
+++ start.sh
@@ -1,24 +1,22 @@
 # Set common variables
-# model="meta-llama/Meta-Llama-3-8B"
-#deepseek-ai/DeepSeek-R1-Distill-Llama-8B
+model="meta-llama/Meta-Llama-3-8B"
 # model="lmsys/vicuna-13b-v1.5"
-# CUDA_LAUNCH_BLOCKING=1 python analyzer.py
-model="liuhaotian/llava-v1.5-13b"
+# model="liuhaotian/llava-v1.5-13b"
 
 CUDA_LAUNCH_BLOCKING=1 python3 main.py \
 --model $model \
---prune_method "structured_afr_llava" \
+--prune_method "snip" \
 --pruning_ratio 0.2 \
 --nsamples 128 \
 --cuda \
---save_model "./pruned_model/LLaVA/AFR-St_0.2p_trimmed2%_global_snipdouble_vision-and-lang" \
---global_pruning
+--save_model "./pruned_model/trash" \
+# --global_pruning
 
-CUDA_LAUNCH_BLOCKING=1 python3 main.py \
---model $model \
---prune_method "structured_afr_llava" \
---pruning_ratio 0.5 \
---nsamples 128 \
---cuda \
---save_model "./pruned_model/LLaVA/AFR-St_0.5p_trimmed2%_global_snipdouble_vision-and-lang" \
---global_pruning
+# CUDA_LAUNCH_BLOCKING=1 python3 main.py \
+# --model $model \
+# --prune_method "structured_afr_llava" \
+# --pruning_ratio 0.5 \
+# --nsamples 128 \
+# --cuda \
+# --save_model "./pruned_model/LLaVA/AFR-St_0.5p_trimmed2%_global_snipdouble_vision-and-lang" \
+# --global_pruning
