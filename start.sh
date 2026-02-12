578867a2629c8277ea4806bef6b14520cc434d8e
RyotaKoba <koba@mprg.cs.chubu.ac.jp>
2026-02-12 12:34:26 +0900

    test

--- start.sh
+++ start.sh
@@ -5,9 +5,9 @@ model="meta-llama/Meta-Llama-3-8B"
 
 CUDA_LAUNCH_BLOCKING=1 python3 main.py \
 --model $model \
---prune_method "structured_afr" \
+--prune_method "afr" \
 --pruning_ratio 0.2 \
---nsamples 128 \
+--nsamples 2 \
 --cuda \
 --save_model "./pruned_model/hoge" \
 # --global_pruning \
