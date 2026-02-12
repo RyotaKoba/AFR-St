e336f3476df6bc25619cb4a477831245c8ac487e
RyotaKoba <koba@mprg.cs.chubu.ac.jp>
2026-02-12 12:47:27 +0900

    fix: shell path

--- test.sh
+++ test.sh
@@ -8,6 +8,6 @@
 ######### model.bin format ###############
 lm-eval \
 --model custom_checkpoint \
---model_args pretrained=meta-llama/Meta-Llama-3-8B,device_map=auto,dtype=float16,checkpoint=../pruned_model/hoge/model.bin \
+--model_args pretrained=meta-llama/Meta-Llama-3-8B,device_map=auto,dtype=float16,checkpoint=./pruned_model/hoge/model.bin \
 --tasks arc_easy,winogrande,hellaswag,arc_challenge,mmlu \
 --batch_size 64
